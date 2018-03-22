84e5a015fe53b635433a260229595e526bb260e4
Smirnov V.S <drolbs@yandex.ru>
2018-03-22 22:07:11 +0400

    add component for adding image

--- web/src/views/Dashboard/Dashboard.js
+++ web/src/views/Dashboard/Dashboard.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import {Row, Col, Card} from 'reactstrap';
+import { Row, Col, Card } from 'reactstrap';
+import Dropzone from 'react-dropzone'
 
 class Dashboard extends Component {
+    render() {
 
-  render() {
-    return (
-      <div className="animated fadeIn">
-        <Row>
-          <Col sm="12" xl="12">
-            Hello world
-          </Col>
-        </Row>
-      </div>
-    )
-  }
+        let dropzoneRef;
+
+        return (
+            <div className="animated fadeIn">
+                <Row>
+                    <Col sm="12" xl="12">
+                        <div>
+                            <Dropzone ref={(node) => { dropzoneRef = node; }} onDrop={(accepted, rejected) => { alert(accepted) }}>
+                                <p>Drop files here.</p>
+                            </Dropzone>
+                            <button type="button" onClick={() => { dropzoneRef.open() }}>
+                                Open File Dialog
+                            </button>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
 }
 
 export default Dashboard;
