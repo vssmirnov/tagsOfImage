a5346f64a1051da2032fddb4bf237821ff9ea93b
Smirnov V.S <drolbs@yandex.ru>
2018-03-27 22:06:15 +0400

    add redirect to /image from /dropzone

--- web/src/views/Dashboard/Dropzone/Dropzone.js
+++ web/src/views/Dashboard/Dropzone/Dropzone.js
@@ -3,14 +3,21 @@ import { Row, Col, Card } from 'reactstrap';
 import Dropzone from 'react-dropzone'
 
 class DropzoneDashboard extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+    }
 
+    goImage() {
+        this.props.history.push('/image');
+    }
+
+    render() {
         let dropzoneRef;
 
         return (
             <div className="animated fadeIn">
                 <div>
-                    <Dropzone className="dropzone" ref={(node) => { dropzoneRef = node; }} onDrop={(accepted, rejected) => { alert(accepted) }}>
+                    <Dropzone className="dropzone" ref={(node) => { dropzoneRef = node; }} onDrop={(accepted, rejected) => { this.goImage() }}>
                         <p>Drop files here.</p>
                     </Dropzone>
                 </div>
